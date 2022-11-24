b6c1a766ad90d122c0189d2ea96d5a4aa677b556
Sahariar Kabir <sronok@gmail.com>
2022-11-24 08:30:41 +0600

    added product route and category

--- index.js
+++ index.js
@@ -18,6 +18,46 @@ console.log(process.env.DB_USER, process.env.DB_PASSWORD);
 
 
 const run = async () => {
+
+    const productCollection = client.db('rgc-db').collection('products');
+    const productCatCollection = client.db('rgc-db').collection('pdctCategory');
+
+
+    //get products 
+    app.get('/products' , async(req ,res) =>{
+        const email = req.query.email
+        const category = req.query.category
+
+        let query ={
+
+        }
+        if(email){
+            query ={
+                email:email
+            }
+        }else if(category){
+            query ={
+                category:category
+            }
+        }
+
+        const result = await productCollection.find(query).toArray();
+        res.send(result)
+    })
+
+    //get products category
+    app.get('/products/category' , async(req ,res) =>{
+        const category = req.query.category
+        let query ={
+
+        }
+
+        const result = await productCatCollection.find(query).toArray();
+        res.send(result)
+    })
+    
+
+
 }
 run().catch(console.dir); 
  
